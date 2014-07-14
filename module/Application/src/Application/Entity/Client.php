9c72f1914b67a8257b0a4cebd901251e25882e0e
lukasz.maslowski <lukasz.maslowski@lm.pl>
2014-07-14 23:36:40 +0200

    new entities

--- module/Application/src/Application/Entity/Client.php
+++ module/Application/src/Application/Entity/Client.php
@@ -2,7 +2,7 @@
 namespace Application\Entity;
 use Doctrine\ORM\Mapping as ORM;
 /** @ORM\Entity */
-class User {
+class Client {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
